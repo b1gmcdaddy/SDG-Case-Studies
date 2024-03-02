cd6be44c245a1248ff6ed0d4ea4a29dacb06e0f2
Jolo <bobtangpuz@gmail.com>
2024-03-02 16:42:30 +0800

    added: pages

---
+++ src/pages/SDG1.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const SDG1 = () => {
+  return <div>SDG1</div>;
+};
+
+export default SDG1;
---
+++ src/pages/SDG2.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const SDG2 = () => {
+  return <div>SDG2</div>;
+};
+
+export default SDG2;
