0a63469a86189929d73f6203c0de9613c75ac386
Jolo <bobtangpuz@gmail.com>
2024-03-11 04:34:40 +0800

    added: researchers section

--- src/pages/SDG1.jsx
+++ src/pages/SDG1.jsx
@@ -1,6 +1,8 @@
 import React, { useState } from "react";
 import Navbar from "../components/Navbar";
 import jebron from "../assets/jebron.jpg";
+import empathymap from "../assets/empathymap.jpg";
+import dev from "../assets/dev.jpg";
 import { faArrowDown } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
@@ -67,9 +69,55 @@ const SDG1 = () => {
           </button>
           {showEmpathyMap && (
             <p className="text-white text-opacity-75 mt-3 text-justify">
-              This is the Empathy Map!
+              This is the Empathy Map!!
             </p>
           )}
+          <img src={empathymap} className="mt-5"></img>
+        </div>
+        <div className="mx-auto mt-10 text-center">
+          <h1 className="text-white md:text-3xl sm:text-2xl tracking-wide text-xl font-bold">
+            MEET THE RESEARCHERS
+          </h1>
+          <div className="w-[100px] h-[4px] mx-auto bg-green-700 rounded-md mt-4 mb-3"></div>
+          <p className="text-white md:mt-2 md:pt-2 md:text-xl xs:text-base">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit duis sed
+            dapibus leonec.
+          </p>
+        </div>
+
+        <div className="w-full py-[4rem] px-4">
+          <div className="max-w-[1240px] mx-auto grid md:grid-cols-3 gap-16">
+            <div className="w-full shadow-xl flex flex-col p-4 my-2 rounded-lg hover:scale-105 duration-300 shadow-gray-400">
+              <img className="mx-auto mt-[-3rem]" src={dev} alt="/" />
+              <h2 className="text-2xl font-bold text-center py-8 text-white">
+                Nikkow Ensomow
+              </h2>
+              <p className="text-center text-base text-white">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis
+                sed dapibus leo nec ornare diam.
+              </p>
+            </div>
+            <div className="w-full shadow-xl flex flex-col p-4 my-4 rounded-lg hover:scale-105 duration-300 shadow-gray-400">
+              <img className="mx-auto mt-[-3rem]" src={dev} alt="/" />
+              <h2 className="text-2xl font-bold text-center py-8 text-white">
+                Sareljohn 5 Pebeda
+              </h2>
+              <p className="text-center text-base text-white">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis
+                sed dapibus leo nec ornare diam.
+              </p>
+            </div>
+            <div className="w-full shadow-xl flex flex-col p-4 my-4 rounded-lg hover:scale-105 duration-300 shadow-gray-400">
+              <img className="mx-auto mt-[-3rem]" src={dev} alt="/" />
+              <h2 className="text-2xl font-bold text-center py-8 text-white">
+                Jolo Tangpuy
+              </h2>
+              <p className="text-center text-base text-white">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis
+                sed dapibus leo nec ornare diam.
+              </p>
+            </div>
+          </div>
         </div>
       </div>
     </>
